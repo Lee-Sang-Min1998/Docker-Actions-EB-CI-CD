bb9d9950688e663d42de1bb2be5ad4ecfb17e7dc
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2023-08-04 11:27:23 +0900

    feat: CI 적용 테스트

---
+++ src/main/java/github/githubaction/controller/GithubActionController.java
@@ -0,0 +1,16 @@
+package github.githubaction.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GithubActionController {
+
+
+
+    @GetMapping("/")
+    public String githubAction(){
+
+        return "hello-build-test";
+    }
+}
